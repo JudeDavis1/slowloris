69daf57efb08cc5b20e8d8c99ca03c2074ea4efc
Jude Davis <judedavis92@gmail.com>
2021-07-01 13:03:23 +0000

    Updated it for noobs

--- slowLoris.py
+++ slowLoris.py
@@ -5,8 +5,8 @@ import socket
 import random
 
 try:
-    host = sys.argv[1]
-    port = 80
+    host = input('Target: ')
+    port = input('Port: ')
     user_agents = [
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
